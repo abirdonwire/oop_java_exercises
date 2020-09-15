86abda37362eb5b79947e9924a03147539ddcedf
Ellie Maidment <ellie947@hotmail.com>
2020-09-15 08:29:16 +0100

    Abstract Cat class defined with private variables, getters and setters

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -1,5 +1,45 @@
 package com.techreturners.cats;
 
-interface Cat {
+public abstract class Cat implements makeAnimal {
+    private boolean isSleeping = false;
+    private String food;
+    private String setting;
+    private int averageHeight;
+} /* The abstract class Cat is showing only the essential details, the related classes (Lion, Cheetah, Domestic Cat)
+will all share these lines of code, and this abstract class will be extended by the related classes. The methods allow us to access
+and modify the state of the Object to which they belong. */
 
+public Cat() {
+
+}
+
+public Cat(String food, String setting, int averageHeight) {
+    this.food = food;
+    this.setting = setting;
+    this.averageHeight = averageHeight;
+}
+
+public String eat() {
+    return this.food;
+}
+
+public String getSetting() {
+    return this.setting; /* Each get method returns the private variables, this is encapsulating all the state data of the Cat object */
+}
+
+public int getAverageHeight() {
+    return this.averageHeight;
+}
+
+public boolean goToSleep() {
+    return this.isSleeping = true; /* Each set method is setting the value of the private variables*/
+}
+
+public boolean wakeUp() {
+    return this.isSleeping = false;
+}
+
+public boolean isSleeping() {
+    return this.isSleeping;
+}
 }
