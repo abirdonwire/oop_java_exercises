cac71115f42592ff63930e900d86c09b3ec62d2f
Ellie Maidment <ellie947@hotmail.com>
2020-09-17 14:52:32 +0100

    Lion class extends Cat

--- src/main/java/com/techreturners/cats/Lion.java
+++ src/main/java/com/techreturners/cats/Lion.java
@@ -2,7 +2,8 @@ package com.techreturners.cats;
 
 public class LionCat extends Cat {
     public LionCat() {
-        super("RRRRRROOOOOOOOOOOOOOOOAAAAAAAAAAAAAAAARRRRRR!!!!", "wild", 1100);
+        super("Roar!!!!", "wild", 1100);
     }
-}/* The Lion child class is inheriting the attributes and methods from the parent Cat class, cats are all very similar so we don't 
+}
+/* The Lion child class is inheriting the attributes and methods from the parent Cat class, cats are all very similar so we don't 
 want to have to re-implement all the shared attributes methods for each one */
