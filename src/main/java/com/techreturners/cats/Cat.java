58e16333346bb192c8dbad2200e1bd44ef344447
Ellie Maidment <ellie947@hotmail.com>
2020-09-27 11:18:41 +0100

    abstract class Cat implements Animal

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -1,51 +1,10 @@
 package com.techreturners.cats;
 
-public interface Cat {
-String eat();
-String getSetting();
-int getAverageHeight();
-boolean goToSleep();
-boolean wakeUp();
-boolean isAsleep();
-}
-
-
-public abstract class AbstractCat implements Cat {
+public abstract class Cat implements Animal {
     private boolean isAsleepNow = false;
     private String food;
     private String setting;
     private int averageHeight;
-} 
 
-public AbstractCat() {}
-
-public AbstractCat(String food, String setting, int averageHeight) {
-    this.food = food;
-    this.setting = setting;
-    this.averageHeight = averageHeight;
-}
-
-public String eat() {
-    return this.food;
-}
-
-public String getSetting() {
-    return this.setting; 
-}
-
-public int getAverageHeight() {
-    return this.averageHeight;
-}
-
-public boolean goToSleep() {
-    return this.isSleeping = true; 
-}
-
-public boolean wakeUp() {
-    return this.isAleepNow = false;
-}
-
-public boolean isSleeping() {
-    return this.isAleepNow;
-}
+    
 }
