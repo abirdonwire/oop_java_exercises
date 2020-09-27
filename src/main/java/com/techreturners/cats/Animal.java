7f6c2b075200f6c64dc4d2eecb2ac45bd950af8d
Ellie Maidment <ellie947@hotmail.com>
2020-09-27 10:51:56 +0100

    rename Animal.java

---
+++ src/main/java/com/techreturners/cats/Animal.java
@@ -0,0 +1,11 @@
+package com.techreturners.cats;
+
+public interface makeAnimal {
+    String eat();
+    String getSetting();
+    int getAverageHeight();
+    boolean goToSleep();
+    boolean wakeUp();
+    boolean isAsleep();
+}
+
