28d6fd41cf3c7964c9a9a5e77f79503a2330526d
Ellie Maidment <ellie947@hotmail.com>
2020-09-27 11:36:40 +0100

    CheetahCat extends Cat

--- src/main/java/com/techreturners/cats/Cheetah.java
+++ src/main/java/com/techreturners/cats/Cheetah.java
@@ -4,5 +4,4 @@ public class CheetahCat extends Cat {
     public CheetahCat() {
         super("Zzzzzzz", "wild", 90);
     }
-}/* Cheetah inherits the behaviour of the Cat class and uses Polymorphism to update the state and behaviour of eating, setting and averageHeight
-because all cats eat and sleep, but they aren't all asleep at the same time and they don't eat the same way. And they are all different sizes.*/
+}
